c8e73d8762a7341be207ed7e8741dd26fa10b585
Annabelle Plein <nnpl0381@hochschule-trier.de>
2025-11-01 14:12:11 +0100

    Initial commit - Unity project with two Scenes

---
+++ Assets/Palettes/MyRandomTiles.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+/*
+[CreateAssetMenu(fileName = "New Random Tile", menuName = "Tiles/Random Tile")]
+public class MyRandomTiles : RandomTile { }
+*/
