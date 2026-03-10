e60ff6a7df4d16262545e49ff133de032c67af1d
Jean Kremer <kremerjean00@gmail.com>
2026-03-10 20:25:49 +0100

    Bugs gefixed

--- Assets/Scripts/Traps/FallingSpike.cs
+++ Assets/Scripts/Traps/FallingSpike.cs
@@ -3,11 +3,11 @@ using UnityEngine;
 public class FallingSpike : MonoBehaviour
 {
     Rigidbody2D rb;
-    [SerializeField] private PolygonCollider2D physicsCollider; // Für den Boden
-    [SerializeField] private PolygonCollider2D triggerCollider; // Für den Schaden am Spieler
-    public float speed;
+    [SerializeField] private LayerMask playerLayer;
     public float distance;
+    public float speed;
     bool isFalling = false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,19 +15,15 @@ public class FallingSpike : MonoBehaviour
 
     void Update()
     {
-        Physics2D.queriesStartInColliders = false; //Damit der Raycast nicht mit dem eigenen Collider kollidiert
         if(isFalling == false)
         {
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance);
-            //Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, distance, playerLayer);
+            Debug.DrawRay(transform.position, Vector2.down * distance, Color.red);
 
-            if(hit.transform != null)
+            if(hit.collider != null) // hit.collider reicht hier völlig aus
             {
-                if(hit.transform.CompareTag("Player")) //Wenn Raycsast den Spieler trifft, fällt der Spike
-                {
-                    rb.gravityScale = speed;
-                    isFalling = true;
-                }
+                rb.gravityScale = speed;
+                isFalling = true;
             }
         }
     }
