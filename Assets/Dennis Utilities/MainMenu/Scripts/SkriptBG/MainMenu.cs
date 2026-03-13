6b97f4598c6fe9c317d8b75871b6c4f641e49fe7
Katzaklysmus <christdennis906@gmail.com>
2026-03-13 10:49:30 +0100

    Safety Measures
    NewGame und Delete Safety Measures!

--- Assets/Dennis Utilities/MainMenu/Scripts/SkriptBG/MainMenu.cs
+++ Assets/Dennis Utilities/MainMenu/Scripts/SkriptBG/MainMenu.cs
@@ -8,11 +8,26 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Notify loadingFailed;
     [SerializeField] private Notify deletingFailed;
     [SerializeField] private Notify deletingSucceded;
+
+    public GameObject quitUI;
+    public GameObject mainUI;
+    public GameObject newGameUI;
     public void StartGame()
     {
-        //load the game scene
-        myLevelLoader.GetComponent<LevelLoaderScript>().LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); //Game Scene
+        
+        PlayerData data = SaveSystem.LoadData();
+        if(data == null)
+        {
+            myLevelLoader.GetComponent<LevelLoaderScript>().LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            CreateNewGame();
+        }
+
+
+        
+
     }
 
     public void LoadGame()
@@ -35,23 +50,53 @@ public class MainMenu : MonoBehaviour
 
     public void DeleteData()
     {
-        try
-        {
-
-            SaveSystem.DeleteData();
-            NotifyManager.ManagerInstance.ShowNotification(deletingSucceded);
-        }
-        catch(System.Exception e)
-        {
-            NotifyManager.ManagerInstance.ShowNotification(deletingFailed);
-        }
-        //Debug.Log("Optionmenu is not Implemented yet...");
-        Debug.Log("Data deleted");
+        DeleteSaveFile();
     }
     public void QuitGame()
     {
         Debug.Log("Quitting Game...");
         Application.Quit();
     }
+    public void DeleteSaveFile()
+    {
+        quitUI.SetActive(true);
+        mainUI.SetActive(false);
+    }
 
+    public void CreateNewGame()
+    {
+        newGameUI.SetActive(true);
+        mainUI.SetActive(false);
+    }
+    public void Yes()
+    {
+        try
+        {
+            SaveSystem.DeleteData();
+            NotifyManager.ManagerInstance.ShowNotification(deletingSucceded);
+        }
+        catch (System.Exception e)
+        {
+            NotifyManager.ManagerInstance.ShowNotification(deletingFailed);
+        }
+        No();
+        Debug.Log("Data deleted...");
+    }
+
+    public void No()
+    {
+        mainUI.SetActive(true);
+        quitUI.SetActive(false);
+    }
+
+    public void YesGame()
+    {
+        myLevelLoader.GetComponent<LevelLoaderScript>().LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NoGame()
+    {
+        mainUI.SetActive(true);
+        newGameUI.SetActive(false);
+    }
 }
