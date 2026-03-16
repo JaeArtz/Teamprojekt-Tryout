2c8591b9522963b568b5e7c6f9753852d6fe68cd
Jean Kremer <kremerjean00@gmail.com>
2026-03-16 19:16:02 +0100

    Climbing Elemente geupdatet mit Top und Dersert Level 1 WIP

--- Assets/Scripts/Traps/EnemyProjectile.cs
+++ Assets/Scripts/Traps/EnemyProjectile.cs
@@ -39,7 +39,7 @@ public class EnemyProjectile : EnemyDamage
 
     private void OnTriggerEnter2D(Collider2D other) 
     {
-        base.OnTriggerStay2D(other); // Basisklassen-Logik für Schaden anwenden
+        base.OnTriggerEnter2D(other); // Basisklassen-Logik für Schaden anwenden
         if (other.CompareTag("Wall") || other.CompareTag("Ground") || other.CompareTag("Player") || other.CompareTag("Lightball"))
         {
             ReturnToPool();
