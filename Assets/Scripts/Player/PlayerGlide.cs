0c0add9069ecc8e15f36542988b145af00cdf5c8
Goodie <98417393+Goodie2@users.noreply.github.com>
2026-03-29 15:19:57 +0200

    Player Gliding
    Added Player Gliding Script

---
+++ Assets/Scripts/Player/PlayerGlide.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class PlayerGlide : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public Rigidbody2D Body { set { body = value; } }
+    private PlayerRoll roll;
+    public PlayerRoll Roll { set { roll = value; } }
+    public bool IsGrounded { get; set; }
+    public bool IsGliding { get; private set; }
+
+    [SerializeField, Tooltip("The gliding velocity at the start.")]
+    private float startingGlidingVelocityY = -1;
+    
+    [SerializeField, Tooltip("The gliding velocity after the glide speed transition time has passed.")]
+    private float endingGlidingVelocityY = -20;
+    
+    [SerializeField, Tooltip("The time needed to go from startingGlideVelocityY to endingGlidingVelocityY.")]
+    private float glideSpeedTransitionDuration = 5;
+    private float glideSpeedTransitionTimer;
+
+    private float currentGlidingVelocityY;
+
+    [SerializeField, Tooltip("The time needed to go from startingGlideVelocityY to endingGlidingVelocityY.")]
+    private float glideAbortionPenalty = 0.25f;
+
+
+    private bool isHoldingSpace;
+    private bool canGlide;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        glideSpeedTransitionTimer = glideSpeedTransitionDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isHoldingSpace = Input.GetKey(KeyCode.Space);
+
+        if (Input.GetKeyUp(KeyCode.Space) && IsGliding && glideSpeedTransitionTimer > 0 && body.linearVelocityY < currentGlidingVelocityY)
+            glideSpeedTransitionTimer = Mathf.Max(0, glideSpeedTransitionTimer - glideAbortionPenalty);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            canGlide = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!body) return;
+
+        UpdateGlidingStatus();
+        float t = 1 - glideSpeedTransitionTimer / glideSpeedTransitionDuration;
+        currentGlidingVelocityY = Mathf.Lerp(startingGlidingVelocityY, endingGlidingVelocityY, t);
+        if (IsGliding)
+        {
+            if (glideSpeedTransitionTimer > 0)
+                glideSpeedTransitionTimer -= Time.fixedDeltaTime;
+
+            body.linearVelocityY = currentGlidingVelocityY;
+        }
+        if (IsGrounded) ResetGlideSpeedTransitionTimer();
+    }
+
+    private void UpdateGlidingStatus()
+    {
+        if (IsGrounded)
+        {
+            if (body.linearVelocityY <= 0)
+                canGlide = false;
+            IsGliding = false;
+            return;
+        }
+        if (!isHoldingSpace)
+        {
+            IsGliding = false;
+            return;
+        }
+        if (roll && roll.IsRolling)
+        {
+            IsGliding = false;
+            return;
+        }
+
+        if (body.linearVelocityY > currentGlidingVelocityY)
+        {
+            IsGliding = false;
+            return;
+        }
+        else
+        {
+            IsGliding = canGlide;
+            return;
+        }
+
+    }
+
+    public void ResetGlideSpeedTransitionTimer()
+    {
+        glideSpeedTransitionTimer = glideSpeedTransitionDuration;
+    }
+}
