97f36516b54b3265b8046595d0c4e0d5283388c8
Katzaklysmus <christdennis906@gmail.com>
2026-04-05 16:52:50 +0200

    Audio Changes when Levels are still locked

--- Assets/Dennis Utilities/MainMenu/LevelSelection/LevelSelection.cs
+++ Assets/Dennis Utilities/MainMenu/LevelSelection/LevelSelection.cs
@@ -2,18 +2,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 public class LevelSelection : MonoBehaviour
 {
     private GameObject myLevelLoader;
-
+    private LoadMenu myLoadMenu;
     public Button[] myButtons;
+    public EventTrigger[] myTriggers;
     public void Awake()
     {
         myLevelLoader = GameObject.Find("LevelLoader");
-
+        myLoadMenu = GameObject.Find("Canvas").GetComponent<LoadMenu>();
         for (int i = 0; i < myButtons.Length; i++)
         {
             myButtons[i].interactable = false;
+            myTriggers[i].enabled = false;
+
         }
         if (SaveSystem.LoadLevelData() != null)
         {
@@ -21,15 +25,18 @@ public class LevelSelection : MonoBehaviour
             foreach (int level in unlockedLevels)
             {
                 myButtons[level].interactable = true;
+                myTriggers[level].enabled = true;
             }
         }
         else
         {
             myButtons[0].interactable = true;
+            myTriggers[0].enabled = true;
         }
     }
     public void OpenLevel(int levelID)
     {
+        myLoadMenu.PlayClickSound();
         myLevelLoader.GetComponent<LevelLoaderScript>().LoadScene(SceneManager.GetActiveScene().buildIndex + levelID);
     }
 }
